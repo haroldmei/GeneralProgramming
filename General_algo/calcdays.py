78b71343048f4042294f26737b3b98890eee07b8
harold mei <haroldmei.cn@gmail.com>
2021-06-26 10:46:10 +1000

    add comment

--- General_algo/calcdays.py
+++ General_algo/calcdays.py
@@ -1,3 +1,16 @@
+'''
+The first and the last day are considered partial days and never counted. Following this
+logic, the distance between two related events on 03/08/2018 and 04/08/2018 is 0,
+since there are no fully elapsed days contained in between those, and 01/01/2000 to
+03/01/2000 should return 1.
+The solution needs to cater for all valid dates between 01/01/1901 and 31/12/2999.
+
+Test cases
+1) ​ 02/06/1983 - 22/06/1983​ = 19 days
+2) ​ 04/07/1984 - 25/12/1984​ = 173 days
+3) ​ 03/01/1989 - 03/08/1983​ = 1979 days
+'''
+
 import argparse
 
 # for simplicity
