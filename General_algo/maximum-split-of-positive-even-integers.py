604f7fb0f3f32522acb9bf1ed4874181fa5e48f0
haroldmei <haroldmei.cn@gmail.com>
2022-04-25 15:57:29 +1000

    not quite sure about it

--- General_algo/maximum-split-of-positive-even-integers.py
+++ General_algo/maximum-split-of-positive-even-integers.py
@@ -1,30 +1,25 @@
-from typing import List
-
-# time out error
-
 class Solution:
-    def __init__(self):
-        self.max_split = 0
-        self.result = None
-    def split(self, max_num, l_even, final):
-        if final == 0 and max_num > self.max_split:
-            self.max_split = max_num
-            self.result = l_even
-                
-        for i in range(1, final//2 + 1):
-            ii = i*2
-            if ii not in l_even and ii <= final:
-                ll = l_even.copy()
-                ll.append(ii)
-                #print(ll, ii, final)
-                self.split(max_num + 1, ll, final - ii)
+    def split(self, curr, result, lst, final):
+        #for i in lst:
+        i = lst[0]
+        if i < final:
+            lst1 = lst.copy()
+            lst1.remove(i)
+            curr.append(i)
+            self.split(curr, result, lst1, final - i)
+        elif i == final:
+            curr.append(final)
+        else:
+            curr[-1] = curr[-1] + final
+            
             
     def maximumEvenSplit(self, finalSum: int) -> List[int]:
         if finalSum % 2 > 0:
             return []
         
-        max_num = 0
-        l = []
-        self.split(max_num, l, finalSum)
-        return self.result
-        
+        lst = [i * 2 for i in range(1, finalSum//2+1)]
+        curr = []
+        result = []
+        self.split(curr, result, lst, finalSum)
+        #print(curr)
+        return curr
